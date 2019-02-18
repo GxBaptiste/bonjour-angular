88ef86734de12ceb818a2c5f2eebce3734c6033e
Baptiste <smaug.exe@gmail.com>
2019-02-18 15:59:37 +0100

    les boutons sont créés automatiquement

--- src/app/liste-collegues-component/liste-collegues-component.ts
+++ src/app/liste-collegues-component/liste-collegues-component.ts
@@ -9,14 +9,32 @@ import { Collegue } from '../models';
 export class ListeColleguesComponent implements OnInit {
 
   @Input() listeCollegues: Collegue[]
-  @Input() val1: number
-  @Input() val2: number
+  debut: number = 0;
+  fin: number = 3;
+  nbrButton: number;
+  tabButton = [];
+
+  clic1(nb: number) {
+    console.log(nb)
+    this.debut = 3 * nb;
+    this.fin = 3 + 3 * nb;
+  }
+
+  remplirTab(nbr: number) {
+
+    for (let i = 0; i < nbr; i++) {
+      this.tabButton.push(i);
+    }
+  }
 
   constructor() { }
 
   ngOnInit() {
+    this.nbrButton = Math.floor(this.listeCollegues.length / 3);
+    if (this.listeCollegues.length % 3 != 0 || this.nbrButton === 0) {
+      this.nbrButton += 1;
+    }
+    this.remplirTab(this.nbrButton);
   }
-
-
 
 }
